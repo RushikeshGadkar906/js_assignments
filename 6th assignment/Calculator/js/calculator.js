a2e4c06ec3fff69f3a2f8db0d8da097907dfc529
rushikeshgadkar <gadkarrushikesh906@gmail.com>
2022-03-07 18:51:48 +0530

    added

--- 6th assignment/Calculator/js/calculator.js
+++ 6th assignment/Calculator/js/calculator.js
@@ -1,8 +1,5 @@
 var calculate = document.getElementById("calculate");
-<<<<<<< HEAD
 var aaa = document.getElementById("res");
-=======
->>>>>>> 4d76ef6908185e5f8eca4f6a69fbb19187026782
 
 $("input").click(function() {
     myNumVal = $(this).val();
@@ -10,24 +7,17 @@ $("input").click(function() {
     calculate.value += myNumVal;
 });
 
-<<<<<<< HEAD
 function calResult() {
     document.getElementById("historyBlock").style.display = "block";
     let myNum = calculate.value;
     // console.log("Called" + myNum);
     let a = myNum;
-=======
-function result() {
-    let myNum = calculate.value;
-
->>>>>>> 4d76ef6908185e5f8eca4f6a69fbb19187026782
     if (myNum == "")
         alert("Enter Number")
     else {
         // console.log(myNum);
         result = eval(myNum);
         calculate.value = result;
-<<<<<<< HEAD
 
         var op = document.createElement("p");
         op.innerHTML = a + " = " + result;
@@ -35,21 +25,10 @@ function result() {
 
     }
 
-=======
-    }
->>>>>>> 4d76ef6908185e5f8eca4f6a69fbb19187026782
 }
 
 function Clear() {
     myNum = "";
     calculate.value = "";
     // console.log(myNum);
-<<<<<<< HEAD
-}
-
-function clearHis() {
-    var removeDiv = document.getElementById("res");
-    removeDiv.parentNode.removeChild(removeDiv);
-=======
->>>>>>> 4d76ef6908185e5f8eca4f6a69fbb19187026782
 }
